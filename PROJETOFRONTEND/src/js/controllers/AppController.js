0310e2e24c553636a64466bcaf5c73806da8b94f
GrazyVaranda <vieiravarandagrazielly@gmail.com>
2024-04-15 12:22:31 -0300

    feat: I added a console log

--- PROJETOFRONTEND/src/js/controllers/AppController.js
+++ PROJETOFRONTEND/src/js/controllers/AppController.js
@@ -12,7 +12,7 @@ export class AppController {
                 this.mostraMenuDrawer();
             });
         }
-        console.log("Javascript foi carregado e iniciado")
+        console.log("Javascript foi carregado e iniciado.")
     }
 
     mostraMenuDrawer() {
